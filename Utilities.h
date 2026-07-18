5cb599611273ac93095ec67ad40ebe098559c2d7
klovely2006 <KacyHines@my.unt.edu>
2026-07-18 15:57:53 -0500

    Add Utilities.h with string manipulation functions

---
+++ Utilities.h
@@ -0,0 +1,17 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+string toLowerCase(string input);
+
+string removePunctuation(string input);
+
+string trim(string input);
+
+#endif
